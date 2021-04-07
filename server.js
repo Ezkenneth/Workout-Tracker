0516602c11caa4dbae47d853acf76e7b4947dbed
Ezkenneth <kennethjoeezigbo@gmail.com>
2021-04-07 16:54:40 -0400

    finished API routing

--- server.js
+++ server.js
@@ -34,6 +34,52 @@ app.get('/stats', (_req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'stats.html'))
 })
 
+// API routes to targer
+  
+
+app.get("/api/workouts", (_, res) => {
+  User.find()
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", (_, res) => {
+  User.create({})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id", ({body, params}, res) => {
+  User.findByIdAndUpdate(params.id, 
+    {$push:{exercises:body}}, 
+    {new:true, runValidators: true})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+}); 
+
+
+app.get('/api/workouts/range', (_, res) => {
+  User.find()
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
